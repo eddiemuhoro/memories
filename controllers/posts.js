e7cee65ae4e36bda8ba34dff6984379816d2112b
Edwin Muhoro <eddiemuhoro@gmail.com>
2022-08-25 13:53:18 +0300

    created post function

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,3 +10,14 @@ export const getPosts = async (req, res) => {
     //added new code
     // res.send('This works!')
 }
+
+export const createPost = async (req, res)=>{
+    const post = req.body;
+    const newPost = new PostMessage(post)
+    try{
+        await newPost.save()
+        res.status(201).json(newPost)
+    }catch{
+        res.status(409).json({message: error.message})
+    }
+}
